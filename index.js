6c2c221d2febae5e05c75a7d8d303a5a7a3f9e84
Roman-js <liverkuzen1989@gmail.com>
2020-07-26 21:10:09 +0300

    Connect to heroku

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    requireTLS: true,
+    auth: {
+        user: 'rdmytrenko07@gmail.com', // generated ethereal user
+        pass: 'Liverkuzen1989', // generated ethereal password
+     },
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/sendMessage', async function (req, res) {
+    // send mail with defined transport object
+    let {message, contacts, name} = req.body;
+    let info = await transporter.sendMail({
+        from: 'HRs WANTS ME', // sender address
+        to: "liverkuzen1989@gmail.com", // list of receivers
+        subject:" HRs WANTS ME", // Subject line
+        /* text: "Hello world?", // plain text body*/
+        html: `<b>HСообщение с вашего портфолио</b>
+               <div>name: ${name}</div>
+               <div>contacts: ${contacts}</div>
+               <div>message: ${message}</div>`, // html body
+    });
+
+    res.send(req.body);
+});
+
+app.listen(3009, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+
+
