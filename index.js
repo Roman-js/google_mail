ae4442adb8c85d077643c7f509ca6d39a31d3cfe
Roman-js <liverkuzen1989@gmail.com>
2020-07-27 18:00:29 +0300

    change get to post

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var subject = 'Hello World from the SendGrid Node.js Library!';
 var content = new helper.Content('text/plain', 'Hello, Email!');
 var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var sg = require('sendMessage')(process.env.SENDGRID_API_KEY);
 var request = sg.emptyRequest({
     method: 'POST',
     path: '/sendMessage',
