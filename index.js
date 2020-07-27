79ec5525a9e004c8fdfb238af1bd9c52d7ab7765
Roman-js <liverkuzen1989@gmail.com>
2020-07-27 17:58:12 +0300

    change get to post

--- index.js
+++ index.js
@@ -14,33 +14,66 @@ var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 var request = sg.emptyRequest({
     method: 'POST',
     path: '/sendMessage',
-    body: {
-        personalizations: [
-            {
-                to: [
-                    {
-                        email: 'liverkuzen1989@gmail.com',
-                    },
-                ],
-                subject: 'Hello World from the SendGrid Node.js Library!',
-            },
-        ],
-        from: {
-            email: 'rdmytrenko07@gmail.com',
-        },
-        content: [
-            {
-                type: 'text/plain',
-                value: 'Hello, Email!',
-            },
-        ],
-    },
+    body: mail.toJSON(),
 });
 
 sg.API(request, function(error, response) {
     console.log(response.statusCode);
     console.log(response.body);
     console.log(response.headers);
+});
+
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+let smtp_login = process.env.SMTP_LOGIN || '';
+let smtp_password = process.env.SMTP_PASSWORD || '';
+
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    requireTLS: true,
+    auth: {
+        user: from_email, // generated ethereal user
+        pass: 'Liverkuzen1989', // generated ethereal password
+     },
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/sendMessage', async function (req, res) {
+    // send mail with defined transport object
+
+    let {message, contacts, name} = req.body;
+    let info = await transporter.sendMail({
+        from: 'HRs WANTS ME', // sender address
+        to: to_email, // list of receivers
+        subject:" HRs WANTS ME", // Subject line
+        /* text: "Hello world?", // plain text body*/
+        html: `<b>HСообщение с вашего портфолио</b>
+               <div>name: ${name}</div>
+               <div>contacts: ${contacts}</div>
+               <div>message: ${message}</div>`, // html body
+    });
+
+
+    res.send(req.body);
+});
+
+let port = process.env.PORT || '';
+
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
 });
 
 
