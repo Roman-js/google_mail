568a153c34c459e187a324f6f1fb28c55307e052
Roman-js <liverkuzen1989@gmail.com>
2020-07-28 15:51:24 +0300

    change get to post1

--- index.js
+++ index.js
@@ -13,28 +13,44 @@ let smtp_login = process.env.SMTP_LOGIN || '';
 let smtp_password = process.env.SMTP_PASSWORD || '';
 
 
-
-let transporter = nodemailer.createTransport({
-
+/*let transporter = nodemailer.createTransport({
 
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
-    /*requireTLS: true,*/
-    /*host: 'gmail',*/
+    requireTLS: true,
+    /!*host: 'gmail',*!/
     auth: {
-        type: 'OAuth2',
         user: 'rdmytrenko07@gmail.com', // generated ethereal user
-        /*pass: 'Liverkuzen1989',*/ // generated ethereal password
+        pass: 'Liverkuzen1989', // generated ethereal password
      },
+});*/
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'OAuth2'
+    }
 });
 
-transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
+transporter.set('oauth2_provision_cb', (user, renew, callback) => {
     let accessToken = userTokens['rdmytrenko07@gmail.com'];
     if(!accessToken){
         return callback(new Error('Unknown user'));
     }else{
         return callback(null, accessToken);
+    }
+});
+
+transporter.sendMail({
+    from: 'rdmytrenko07@gmail.com',
+    to: 'liverkuzen1989@gmail.com',
+    subject: 'Message',
+    text: 'I hope this message gets through!',
+    auth: {
+        user: 'user@example.com'
     }
 });
 
