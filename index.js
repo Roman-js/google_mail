72842be807ff3b2593815b87c5a00cb646d8c368
Roman-js <liverkuzen1989@gmail.com>
2020-07-28 21:06:25 +0300

    try to send message to email from heroku

--- index.js
+++ index.js
@@ -17,13 +17,12 @@ let smtp_password = process.env.SMTP_PASSWORD || 'Liverkuzen1989';
 
 let transporter = nodemailer.createTransport({
 
-    host: 'smtp-relay.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    requireTLS: true,
+    // host: 'smtp.gmail.com',
+    // port: 587,
+    // secure: false, // true for 465, false for other ports
+    // requireTLS: true,
 
-   /*service: 'gmail',*/
-
+   service: 'gmail',
     auth: {
         user: smtp_login, // generated ethereal user
         pass: smtp_password, // generated ethereal password
