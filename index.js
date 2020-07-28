477a3086922d4dc4a45da411d05b861cc42921fd
Roman-js <liverkuzen1989@gmail.com>
2020-07-28 15:39:39 +0300

    change get to post1

--- index.js
+++ index.js
@@ -12,29 +12,30 @@ app.use(bodyParser.json());*/
 let smtp_login = process.env.SMTP_LOGIN || '';
 let smtp_password = process.env.SMTP_PASSWORD || '';
 
-let accessToken = transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
-    let accessToken = userTokens['rdmytrenko07@gmail.com'];
-    if(!accessToken){
-        return callback(new Error('Unknown user'));
-    }else{
-        return callback(null, accessToken);
-    }
-});
+
 
 let transporter = nodemailer.createTransport({
 
 
     host: 'smtp.gmail.com',
     port: 587,
-    secure: false, // true for 465, false for other ports
-    requireTLS: true,
+    secure: true, // true for 465, false for other ports
+    /*requireTLS: true,*/
     /*host: 'gmail',*/
     auth: {
         type: 'OAuth2',
         user: 'rdmytrenko07@gmail.com', // generated ethereal user
         /*pass: 'Liverkuzen1989',*/ // generated ethereal password
-        accessToken: accessToken
      },
+});
+
+transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
+    let accessToken = userTokens['rdmytrenko07@gmail.com'];
+    if(!accessToken){
+        return callback(new Error('Unknown user'));
+    }else{
+        return callback(null, accessToken);
+    }
 });
 
 app.get('/', function (req, res) {
